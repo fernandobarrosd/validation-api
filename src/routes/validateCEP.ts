0ff0b511ffc9e3530e5f2cc450f224018342f04b
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-02-15 17:06:32 -0300

    Create validateCEP route

---
+++ src/routes/validateCEP.ts
@@ -0,0 +1,33 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+
+export default async function(app: FastifyInstance) {
+    app.post("/cep", (request, reply) => {
+        const validateCEPBody = z.object({
+            cep: z.string()
+        });
+        const result = validateCEPBody.safeParse(request.body);
+
+        if (!result.success) {
+            return reply.status(400).send({
+                message: result.error.message,
+                statusCode: 400
+            });
+        }
+        const { data: { cep } } = result;
+
+        const isCEP = /^[0-9]{5}-[0-9]{3}$/.test(cep);
+
+        if (isCEP) {
+            return reply.send({
+                message: "CEP is valid",
+                cep
+            });
+        }
+        return reply.status(400).send({
+            message: "CEP is invalid",
+            cep,
+            statusCode: 400
+        });
+    });
+}
