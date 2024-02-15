df845e0cfd73861c441b4a1092cb1dfdbe9ba7d7
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-02-15 17:07:11 -0300

    Create validateCreditCard route

---
+++ src/routes/validateCreditCard.ts
@@ -0,0 +1,33 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+
+export default async function(app: FastifyInstance) {
+    app.post("/creditCard", (request, reply) => {
+        const validateCPFBody = z.object({
+            creditCard: z.string()
+        });
+        const result = validateCPFBody.safeParse(request.body);
+
+        if (!result.success) {
+            return reply.status(400).send({
+                message: result.error.message,
+                statusCode: 400
+            });
+        }
+        const { data: { creditCard } } = result;
+
+        const isCreditCard = /^[0-9]{4}\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}$/.test(creditCard);
+
+        if (isCreditCard) {
+            return reply.send({
+                message: "Credit Card number is valid",
+                creditCard
+            });
+        }
+        return reply.status(400).send({
+            message: "Credit Card number is invalid",
+            creditCard,
+            statusCode: 400
+        });
+    });
+}
