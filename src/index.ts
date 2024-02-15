bdd41aef062f7026575d5facd7ac6ba7e49c404f
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-02-15 15:36:18 -0300

    Init server

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import fastifyAutoload from "@fastify/autoload";
+import fastify from "fastify";
+import { join } from "node:path";
+
+const PORT = process.env.PORT || 3000;
+const app = fastify();
+
+
+app.register(fastifyAutoload, {
+    dir: join(__dirname, "routes")
+});
+
+
+app.listen({ port: PORT !== 3000 ? parseInt(PORT) : PORT }).then(() => {
+    console.log(`Server is running on http://localhost:${PORT}/`)
+})
